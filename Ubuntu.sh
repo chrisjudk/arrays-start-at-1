a6648e3fb97a515d14452daf5edd5742b7ea9687
chrisjudk <chrisjudkins@live.com>
2017-09-06 01:22:02 -0400

    added outputs for what the command is doing and change cjudkins.log to ASAO.log

--- Ubuntu.sh
+++ Ubuntu.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 # Get updates
+echo "getting updates"
 apt-get update
 # Install Uncomplicated Firewall (UFW)
+echo "installing Uncomplicated firewall"
 apt-get install ufw -y
 # Upgrade all installed packages
+echo "installing updates"
 apt-get upgrade -y
 # Clean up unnecessary junk
+echo "running autoclean and autoremove"
 apt-get autoclean
 apt-get autoremove -y
 # enable UFW
+echo "enabling UFW"
 ufw enable
 # make log dir
+echo "creating /var/local"
 mkdir /var/local/
 # Create/clear log file
-echo -n "" > /var/local/cjudkins.log
+echo "creating log file in /var/local"
+echo -n "" > /var/local/ASAO.log
 # Add additional instructions to log file
-echo "getent group <groupname> |||| Users in group" >> /var/local/cjudkins.log
-echo "/etc/pam.d/common-password	minlen-8 |||| password length" >> /var/local/cjudkins.log
-echo "/etc/login.defs |||| Password expiration" >> /var/local/cjudkins.log
-echo "/etc/ssh/sshd_config	PermitRootLogin no' |||| Disable Root Login" >> /var/local/cjudkins.log
+echo "adding instructions to log file"
+echo "getent group <groupname> |||| Users in group" >> /var/local/ASAO.log
+echo "/etc/pam.d/common-password	minlen-8 |||| password length" >> /var/local/ASAO.log
+echo "/etc/login.defs |||| Password expiration" >> /var/local/ASAO.log
+echo "/etc/ssh/sshd_config	PermitRootLogin no' |||| Disable Root Login" >> /var/local/ASAO.log
 echo "Don't Forget to Restart"
 echo "more password stuff @ https://www.cyberciti.biz/tips/linux-check-passwords-against-a-dictionary-attack.html"
 # Install libpam-cracklib which is used to check passwords
+echo "installing libpam-cracklib for passwords"
 apt-get install libpam-cracklib -y
