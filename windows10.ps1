e53a88d9b16edd46637549fb6b38e93c60637ba0
davidpeeler9 <davidpeeler9@gmail.com>
2017-11-15 13:41:11 -0500

    Cleanup

--- windows10.ps1
+++ windows10.ps1
@@ -1,5 +1,10 @@
+http://brandonpadgett.com/powershell/Local-gpo-powershell/
 
-#Finding files
-$path = Read-Host -Prompt 'Input what path you want to search'
-$extensions = Read-Host -Prompt 'Input what extensions you want to search for'
-Get-childitem -Path $path -Recurse -ErrorAction -Include $extensions SilentlyContinue
+################################################################################################################
+
+#Finding Files
+function findFile{
+  $path = Read-Host -Prompt 'Input what path you want to search'
+  $extensions = Read-Host -Prompt 'Input what extensions you want to search for'
+  Get-childitem -Path $path -Recurse -ErrorAction -Include $extensions SilentlyContinue
+}
